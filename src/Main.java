c7ca5c3a708fe6f24c0f09041083d95e60b06f06
Neto Nikolay <nikolay.kastranets.1985@gmail.com>
2023-07-06 21:53:06 +0300

    bonus 2 one

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int score = 10;
+        int replenishment = 1200;
+
+        int bonus;
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println ("Сумма счета " + (score + replenishment + bonus));
+        System.out.println ("Сумма бонуса " + bonus + " рублей");
+    }
+}
+
+
